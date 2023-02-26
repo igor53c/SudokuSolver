ee093d67d9edaee2a0a33e4952c2e204cb1dc426
Igor <nesto@nesto.co>
2023-02-26 17:24:56 +0100

    Initial commit

---
+++ src/SudokuSolver.java
@@ -0,0 +1,67 @@
+public class SudokuSolver {
+    private static final int SIZE = 9;
+    private static final int EMPTY = 0;
+
+    public static void solve(int[][] board) {
+
+        if (solveSudoku(board)) {
+            printBoard(board);
+        } else {
+            System.out.println("Neuspesno resavanje.");
+        }
+    }
+
+    private static boolean solveSudoku(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (board[row][col] == EMPTY) {
+                    for (int num = 1; num <= SIZE; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solveSudoku(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = EMPTY;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isValid(int[][] board, int row, int col, int num) {
+        for (int i = 0; i < SIZE; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
+            }
+        }
+        int boxRow = row - row % 3;
+        int boxCol = col - col % 3;
+        for (int i = boxRow; i < boxRow + 3; i++) {
+            for (int j = boxCol; j < boxCol + 3; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static void printBoard(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            if (row % 3 == 0 && row != 0) {
+                System.out.println("------+-------+------");
+            }
+            for (int col = 0; col < SIZE; col++) {
+                if (col % 3 == 0 && col != 0) {
+                    System.out.print("| ");
+                }
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
